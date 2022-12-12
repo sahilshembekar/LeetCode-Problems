29eb0c47da922929547fbdefb38e1d3231af7294
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:12 +0900

    minStack Algo #2

---
+++ 155-minStack/main2.cpp
@@ -0,0 +1,58 @@
+class MinStack {
+    //Stack can be used 
+    //Algo #2 1-stack approach 
+    
+    stack <int> st;
+    //stack <int> minStack;
+    int min;
+    
+public:
+    MinStack() {
+        
+        min = INT_MAX;
+        //minStack.push(min);
+    }
+    
+    void push(int val) {
+        //  st.push(val); //if you use this here it will not go through the if loop. Pretty logical why...
+        if (val <= min) {
+            st.push(min);            
+            min = val;
+        }
+        st.push(val);
+
+        //min = std::min(min,val);
+        
+    }
+    
+    void pop() {
+        int top = st.top();
+        st.pop();
+        
+        if (top == min) {
+            min = st.top();
+            st.pop();
+        }
+        // st.pop();
+        // minStack.pop();
+        // min = minStack.top();
+        
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return min;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
