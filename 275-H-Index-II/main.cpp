a5e70043791acc432e562cb6db31f2a600c503a2
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:13 +0900

    275. H-Index II

--- 275-H-Index-II/main.cpp
+++ 275-H-Index-II/main.cpp
@@ -19,3 +19,36 @@ public:
         return 0;
     }
 };
+
+//Binary Search
+
+/*
+Time Complexity = O(logn)
+Space Complexity = O(1)
+where n is the number of elements in the vector.
+*/
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        //Need the NULL case here
+        if (citations.size() == 0) return 0;
+        
+        int n = citations.size();
+        int low =0;
+        int high = n-1;
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            if (citations[mid] == n-mid) {
+                return n-mid;
+            }
+            else if (citations[mid] < n-mid) {
+                low = mid+1;
+            }
+            else {
+                high = mid-1;
+            }
+        }
+        return n-low;
+    }
+};
