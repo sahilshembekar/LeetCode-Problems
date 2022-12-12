7ff64d6a40c2a0be5294ff9eb5ec6dec5cffeeee
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:13 +0900

    200. Number of islands

--- 200-Number-of-Islands/main.cpp
+++ 200-Number-of-Islands/main.cpp
@@ -48,3 +48,60 @@ public:
         return count;
     }
 };
+
+
+
+// DFS approach
+/*
+Time Complexity = O(M*N)
+Space Complexity = O(M*N)
+where M is the number of rows and N is the number of coloumns in the matrix.
+*/
+
+class Solution {
+public:
+    int m;
+    int n;
+    vector <vector<int>> dirs{{0,1},{1,0},{-1,0},{0,-1}};
+    int count = 0;
+    
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.size() == 0) return 0;
+        m = grid.size();
+        n = grid[0].size();
+        
+        
+        //Traverse the grid
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == '1') {
+                    //if we come across 1 it is an island
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    void dfs(vector <vector<char>> &grid, int i, int j) { //Make sure to pass by reference
+        //base
+        //return nothing if we are out of bounds or come across  0
+        if (i<0 || j<0 || i>=m || j>=n || grid[i][j] == '0') return;
+        
+        //logic
+        //Make the element itself 0
+        grid[i][j] = '0';
+        //check the neighbours with dirs
+        
+        for (int k =0; k<dirs.size(); k++) {
+            int r = i + dirs[k][0];
+            int c = j + dirs[k][1];
+            //call the dfs and check its neigbours //babies babies babies and so on
+            
+            dfs(grid,r,c);
+            
+            // no boundary check and condition check here we do that in base case 
+        }
+    }
+};
