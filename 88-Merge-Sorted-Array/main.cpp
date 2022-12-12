31fe04c2d608889d675b917d415345cb256fa179
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:12 +0900

    88. Merge Sorted Array

---
+++ 88-Merge-Sorted-Array/main.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/merge-sorted-array/
+
+// 88. Merge Sorted Array
+
+//Time Complexity = O(M+N)
+//Space Complexity = O(1)
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int ptr1 = m-1;
+        int ptr2 = n-1;
+        int i = m+n-1;
+        while (ptr1 >=0 && ptr2 >=0) {
+            if (nums1[ptr1] > nums2[ptr2]) {
+                nums1[i] = nums1[ptr1];
+                ptr1--;
+                }
+            else {
+                nums1[i] = nums2[ptr2];
+                ptr2--;
+            }
+            i--;
+        }
+                        
+        while (ptr2>=0) {
+            nums1[i]=nums2[ptr2];
+            ptr2--;
+            i--;
+        }
+    }
+};
