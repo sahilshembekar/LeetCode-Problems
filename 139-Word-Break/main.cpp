baa9bfa9020c66f845fc32d1577155d7d69fc7b0
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:14 +0900

    139 Word Break

--- 139-Word-Break/main.cpp
+++ 139-Word-Break/main.cpp
@@ -40,3 +40,44 @@ public:
     return false;
     }
 };
+
+
+/*
+TC = O(n^4)
+SC = O(l)
+*/
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        if (s.size() != 0 && wordDict.size() == 0) {
+            return false;
+        }
+        
+        // Make a hashset from the vector of Dict
+        set <string> st; 
+        for(int i = 0; i < wordDict.size(); i++) {
+            st.insert(wordDict[i]);
+        }
+        
+        int n = s.length();
+        
+        vector <bool> dp(n+1, false);
+        
+        dp[0] = true; //root node i.e. first node is a valid split
+        
+        for (int i = 0; i < dp.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if(dp[j]) { //If we have a 1 /true here that means the characters before this contain a valid split
+                    if (st.find(s.substr(j,i-j)) != st.end()) {
+                        // the remaining characters from the j to i also generate a valid string which is present in set
+                        dp[i] = true; 
+                        break; //No need to check again/remaining
+                    }   
+                }
+            }   
+        }
+        return dp[n]; //return the value at last
+    }
+        
+};
