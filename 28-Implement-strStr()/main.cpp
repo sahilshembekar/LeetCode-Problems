8a2a017263e84e62f5c995e74e3f2cfa724d3491
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:13 +0900

    28. Implement strStr()

--- 28-Implement-strStr()/main.cpp
+++ 28-Implement-strStr()/main.cpp
@@ -44,3 +44,77 @@ public:
         return -1; //Otherwise
     }
 };
+
+
+/*
+Time Complexity = O(m+n)
+Space Complexity = O(n)
+where m is the length of the haystack and n is the length of the needle.
+KMP Algorithm used
+*/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.length() == 0 ) {
+            return 0;
+        }
+        if(haystack.length() <needle.length()) {
+            return -1;
+        }
+        
+        int m = haystack.length();
+        int n = needle.length();
+        
+        vector<int> lps (n,0); //(size,default_value)
+        
+        //Make the LPS array for the needle string
+        int i=1,j=0;
+        
+        while(i<n) { //Traverse the needle
+            //3 scenarios are possible 
+            if (needle[i] == needle[j]) {
+                j++;
+                lps[i] =j;
+                i++;
+            }
+            
+            else if (needle[i] != needle[j] && j>0) {
+                //squeeze the window
+                j = lps[j-1];
+            }
+            
+            else { // when needle[i] != needle[j] && j==0
+                lps[i] =0;
+                i++;
+            }
+        } //lps array created
+        
+        //logic
+        
+        i=0,j=0; //reset to start
+        
+        while (i<m) { //Traverse the haystack to search
+            if (needle[j] == haystack[i]) {
+                i++;
+                j++;
+                if (j==n) {
+                    //If everything has matched
+                    return i-n; //returns first index from where we matched 
+                }
+            }
+            else if(needle[j] != haystack[i] && j>0) {
+                j = lps[j-1]; 
+                //move the j to left side
+                //check what is the longest suffix & prefix on the element the the left of where we found the mismatch 
+            }
+            
+            else { // when needle[j] != haystack[i] && j==0
+                i++;
+            }
+        }
+        
+        
+        return -1; //Otherwise
+    }
+};
