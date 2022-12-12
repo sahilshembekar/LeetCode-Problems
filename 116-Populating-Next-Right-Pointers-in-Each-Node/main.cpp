d87fde2859c37845a46f26abb81e5e70744fad9f
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:13 +0900

    116. Populating Next Right Pointers in Each Node

--- 116-Populating-Next-Right-Pointers-in-Each-Node/main.cpp
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/main.cpp
@@ -48,3 +48,74 @@ public:
         
     }
 };
+
+
+// BFS Approach with queue
+// Time Coplexity = O(N)
+// Space Complexity = O(h)
+// where n is the number of nodes in the tree.
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        queue <Node*> q;
+        q.push(root);
+        Node* curr = NULL;
+        
+        while(!q.empty()) {
+            int size = q.size();
+            // first element is taken and popped
+            curr = q.front();
+            q.pop();
+            //Go the babies add them to the queue
+            if(curr->left != NULL) {
+                q.push(curr->left);
+            }
+            if(curr->right != NULL) {
+                q.push(curr->right);
+            }
+            //here we start i from 1
+            for (int i=1; i<size; i++) {
+                Node* temp = q.front();
+                q.pop();
+                
+                curr->next = temp; //Node has next pointer use that to assign values
+                //Link is established
+                curr = temp;
+                
+                //Add the babies to the queue before losing access
+                
+                if(curr->left != NULL) {
+                q.push(curr->left);
+                }
+                
+                if(curr->right != NULL) {
+                q.push(curr->right);
+                }
+            }
+        }
+        return root;
+    }
+};
