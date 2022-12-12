98ba53af59f4e10f2d01b5dfc96a7d57ea972947
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:13 +0900

    227. Basic Calculator II

--- 227-Basic-Calculator-II/main.cpp
+++ 227-Basic-Calculator-II/main.cpp
@@ -73,3 +73,74 @@ public:
         return result;
     }
 };
+
+
+/*
+Time Complexity = O(N)
+Space Complexity = O(1)
+where n is the size of the given string.
+*/
+
+
+class Solution {
+public:
+    int calculate(string s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+
+        //s = s.trim() // Not possible maybe use lambda?
+        
+        int curr=0,calc=0,tail=0;
+        char lastSign = '+';
+        for (int i=0;i<s.length();i++) {
+            //iterate over each character of the string s
+            
+            if(isdigit(s[i])){
+                curr = (curr*10) +(s[i]- '0'); // latter part to get the ascii value of the digit
+            } 
+            
+            if ((!isdigit(s[i]) && s[i] != ' ') || i == s.length()-1) {
+                // we need to skip spaces if they occur
+                //for the last char we have to calc the value which might be a digit hence to pass that through this loop we do || case
+                // thats why if not elif
+                
+                if (lastSign == '+') {
+                    calc = calc+curr;
+                    tail = curr;
+                }
+                
+                if (lastSign == '-') {
+                    calc = calc-curr;
+                    tail = -curr;
+                }
+                
+                if (lastSign == '*') {
+                    //tail is the mess that we create previously
+                    //In order to follow BODMAS/PEMDAS rule
+                    calc = (calc-tail) + (tail*curr);
+                    tail = tail*curr;
+                }
+                // else { //Imporatant follow up if curr becomes 0 then what
+                //     if(curr!=0 ) {
+                //      //tail is the mess that we create previously
+                //      calc = (calc-tail) + (tail/curr);
+                //      tail = tail/curr;
+                //     else {
+                //     return INT_MAX; 
+                //      }
+                //   }
+                //}
+                if (lastSign == '/') {
+                    //tail is the mess that we create previously
+                    calc = (calc-tail) + (tail/curr);
+                    tail = tail/curr;
+                }
+                
+                lastSign = s[i]; // update the lastSign with the current character
+                curr = 0; //reset the curr
+            }
+        }
+        return calc;
+    }
+};
