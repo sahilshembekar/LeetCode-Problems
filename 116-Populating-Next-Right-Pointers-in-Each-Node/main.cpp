56f27b71c3f06c91ffc8036ab97bf26a9d518b34
sahilshembekar <sahil.shembekar@gmail.com>
2022-12-12 22:41:13 +0900

    116. Populating Next Right Pointers in Each Node

--- 116-Populating-Next-Right-Pointers-in-Each-Node/main.cpp
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/main.cpp
@@ -119,3 +119,52 @@ public:
         return root;
     }
 };
+
+// BFS Approach without extra space // fast
+// Time Coplexity = O(N)
+// Space Complexity = O(1)
+// where n is the number of nodes in the tree.
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        
+        Node* level=root;
+        Node* curr= NULL;
+        while(level->left!= NULL) {
+            curr=level;
+            while(curr != NULL) {
+                curr->left->next = curr->right;
+                
+                if (curr->next != NULL) {
+                    curr->right->next = curr->next->left;
+                }
+                curr=curr->next;
+            }
+            level=level->left;
+        }
+        return root;
+    }
+};
